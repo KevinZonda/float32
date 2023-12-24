a214d283a58520ae11822a36080df98fea536520
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-12-24 20:34:19 +0000

    fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {Button, Dropdown, Input} from "tdesign-react";
 import { Icon } from 'tdesign-icons-react';
 import React from "react";
 import {Content} from "./Content.tsx";
+import ReqStore from "./Store/ReqStore.ts";
 
 const langOpt = [
   {
@@ -26,9 +27,52 @@ function langMap(lang: string) {
   }
 }
 
+const proglangOpt = [
+  {
+    content: 'General',
+    value: 'General',
+  },
+  {
+    content: 'Go',
+    value: 'golang',
+  },
+  {
+    content: 'Java',
+    value: 'Java',
+  },
+  {
+    content: 'C#',
+    value: 'C#',
+  },
+  {
+    content: 'C',
+    value: 'C',
+  },
+  {
+    content: 'C++',
+    value: 'C++',
+  },
+  {
+    content: 'Haskell',
+    value: 'Haskell',
+  }
+];
+
+function progLangMap(lang: string) {
+  switch (lang) {
+    case 'general':
+      return 'General';
+    case 'golang':
+      return 'Go';
+    default:
+      return lang;
+  }
+}
 
 function App() {
   const [lang, setLang] = React.useState('zh');
+  const [progLang, setProgLang] = React.useState('general');
+
   return (
     <>
       <h1 style={{fontFamily: 'Linux Libertine'}}><span style={{fontStyle: 'italic'}}>float32</span> AI : Docs 	&times; Elegant</h1>
@@ -38,10 +82,18 @@ function App() {
       <Input
         placeholder="请输入你的问题"
         size="large"
+        onEnter={(e) => {
+          ReqStore.queryQuestion(e, lang, 'golang')
+        }}
       />
       <Dropdown options={langOpt} onClick={(e)=> setLang(e.value as string)}>
         <Button variant="text" suffix={<Icon name="chevron-down" size="16" />}>
           {langMap(lang)}
+        </Button>
+      </Dropdown>
+      <Dropdown options={proglangOpt} onClick={(e)=> setProgLang(e.value as string)}>
+        <Button variant="text" suffix={<Icon name="chevron-down" size="16" />}>
+          {progLangMap(progLang)}
         </Button>
       </Dropdown>
       <div style={{height: '16px'}}></div>
---
+++ frontend/src/Store/ReqStore.ts
@@ -0,0 +1,59 @@
+import {makeAutoObservable} from "mobx";
+const baseAPI = 'http://localhost:8080/query'
+class reqStore {
+  public constructor() {
+    makeAutoObservable(this)
+  }
+
+  private _currentAns: string = ''
+  public get currentAns(): string {
+    return this._currentAns
+  }
+
+  public set currentAns(value: string) {
+    this._currentAns = value
+  }
+
+  private _isLoading: boolean = false
+
+  public get isLoading(): boolean {
+    return this._isLoading
+  }
+
+  public set isLoading(value: boolean) {
+    this._isLoading = value
+  }
+
+  public async queryQuestion(question : string, lang: string, progLang: string) {
+    if (this.isLoading) return
+
+    this.isLoading = true
+    this.currentAns = ''
+
+    const res = await fetch(baseAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'question' : question,
+        'language' : lang,
+        'prog_lang' : progLang
+      })
+    })
+    const reader = res.body!.pipeThrough(new TextDecoderStream()).getReader();
+    /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+    while (true) {
+      const { done, value } = await reader.read();
+      console.log(value)
+      if (value !== undefined) {
+        this.currentAns = this.currentAns + value
+      }
+      if (done) break;
+    }
+    this.isLoading = false
+  }
+
+}
+
+export default new reqStore()
