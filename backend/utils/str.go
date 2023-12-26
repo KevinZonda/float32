3a05225afc0be863516d159b200ceb0915ab9550
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-12-26 09:22:08 +0000

    fix len

--- backend/utils/str.go
+++ backend/utils/str.go
@@ -24,3 +24,19 @@ func StrMaxLen(str string, maxLen int) string {
 	}
 	return string(ss[:maxLen])
 }
+
+func StrMaxLenSmart(str string, maxLen int) string {
+
+	bl := float64(len([]byte(str)))
+	rl := float64(len([]rune(str)))
+	// if is latin, then double len
+	needDouble := (1.0*(bl-rl))/bl < 0.2
+	if needDouble {
+		maxLen = int(2.5 * float64(maxLen))
+	}
+	ss := []rune(str)
+	if len(ss) <= maxLen {
+		return str
+	}
+	return string(ss[:maxLen])
+}
