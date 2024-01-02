f9908d89bc6d4bedc17bba65929e0ef5a1d7181d
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-01-02 18:36:12 +0000

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,13 @@ function useQuery() {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
+const Conditional = ({condition, children}: { condition: boolean, children: React.ReactElement }) => {
+  if (condition) {
+    return children
+  }
+  return <></>
+}
+
 export const App = observer(() => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 720)
   useEffect(() => {
@@ -70,13 +77,15 @@ export const App = observer(() => {
           ReqStore.question = e
         }}
         onEnter={(question, e) => {
-          if (!e.e.nativeEvent.isComposing && question !== '') {
-            BaseStore.lang = lang
-            BaseStore.field = field
-            BaseStore.fieldSpec = fieldSpec
-            BaseStore.question = question
-            ReqStore.queryQuestion(question, lang.query, field.field, fieldSpec.query);
+          if (e.e.nativeEvent.isComposing || question === '') {
+            return
           }
+          BaseStore.lang = lang
+          BaseStore.field = field
+          BaseStore.fieldSpec = fieldSpec
+          BaseStore.question = question
+          ReqStore.queryQuestion(question, lang.query, field.field, fieldSpec.query);
+
         }}
       />
       <Dropdown options={langOpt} onClick={(e) => {
@@ -90,48 +99,46 @@ export const App = observer(() => {
           {lang.content}
         </Button>
       </Dropdown>
-      {
-        fieldsOpt.length > 1 &&
-          <Dropdown options={fieldsOpt} onClick={(e) => {
-            const f = e.value as IField
-            setFieldIcon(f.icon)
-            setSubIcon(f.subIcon)
-            setField(f)
-            setFieldSpec(f.options[0].value)
-            if (f.field === 'med') {
-              ReqStore.warning = '本网站上的信息来自互联网或人工智能生成内容。网站无意提供医疗建议，也不能代替由资质的医生、药剂师或其他医疗保健专业人员所提供的咨询。读者不能因为本网站上提供的某些信息，而无视医生的建议或延迟就医。'
-            } else {
-              ReqStore.warning = ''
-            }
-          }}>
-              <Button style={dropdownBtnStyle} variant="text" icon={fieldIcon}>
-                {field.content}
-              </Button>
-          </Dropdown>
-      }
-      {
-        field.options.length > 1 &&
-          <Dropdown options={field.options}
-                    onClick={(e) => {
-                      const fs = e.value as { query: string, content: string, icon: string }
-                      setFieldSpec(fs)
-                    }}>
-              <Button style={dropdownBtnStyle} variant="text" icon={subIcon}>
-                {fieldSpec.content}
-              </Button>
-          </Dropdown>
-      }
-      {
-        !isMobile && (
-          <Button style={dropdownBtnStyle} theme="default" variant="text"
-                  icon={<ReactIcon><MdOutlineInfo/></ReactIcon>}
-                  onClick={() => {
-                    nav('/about')
-                  }}>
-            关于
+
+      <Conditional condition={fieldsOpt.length > 1}>
+        <Dropdown options={fieldsOpt} onClick={(e) => {
+          const f = e.value as IField
+          setFieldIcon(f.icon)
+          setSubIcon(f.subIcon)
+          setField(f)
+          setFieldSpec(f.options[0].value)
+          ReqStore.warning =
+            f.field === 'med'
+              ? '本网站上的信息来自互联网或人工智能生成内容。网站无意提供医疗建议，也不能代替由资质的医生、药剂师或其他医疗保健专业人员所提供的咨询。读者不能因为本网站上提供的某些信息，而无视医生的建议或延迟就医。'
+              : ''
+        }}>
+          <Button style={dropdownBtnStyle} variant="text" icon={fieldIcon}>
+            {field.content}
           </Button>
-        )
-      }
+        </Dropdown>
+      </Conditional>
+
+      <Conditional condition={field.options.length > 1}>
+        <Dropdown options={field.options}
+                  onClick={(e) => {
+                    const fs = e.value as { query: string, content: string, icon: string }
+                    setFieldSpec(fs)
+                  }}>
+          <Button style={dropdownBtnStyle} variant="text" icon={subIcon}>
+            {fieldSpec.content}
+          </Button>
+        </Dropdown>
+      </Conditional>
+
+      <Conditional condition={!isMobile}>
+        <Button style={dropdownBtnStyle} theme="default" variant="text"
+                icon={<ReactIcon><MdOutlineInfo/></ReactIcon>}
+                onClick={() => {
+                  nav('/about')
+                }}>
+          关于
+        </Button>
+      </Conditional>
 
       <div style={{height: '16px'}}></div>
       <ContentLayout/>
