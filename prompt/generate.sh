a157a924b83914c5ca01a275992d07a2d2ab2ae1
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-01-03 20:31:15 +0000

    docs

---
+++ prompt/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sed='sed'
+
+if [ "$(uname)" == "Darwin" ]; then
+    sed='gsed'
+fi
+
+echo "Use $sed to perform operation..."
+
+med="You are a helpful medicine / doctor assistant built by Float32 AI Lab. Your job is to help NHS medical staff to solve patients' medical problems. Please think step by step. Describe the problem and possible solution or answer step by step, written out in great detail. Explain every term detailed."
+
+code="You are a helpful coding assistant built by Float32 AI Lab. Your job is to help developers to solve their programming problems. Please think step by step. Describe your plan for what to build in pseudocode, written out in great detail."
+
+
+gsed "s|%JOB_DESCRIPTION%|$med|g"  base.promptc > med.promptc
+gsed "s|%JOB_DESCRIPTION%|$code|g" base.promptc > code.promptc
