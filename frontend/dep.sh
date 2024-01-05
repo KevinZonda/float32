8aa8491a32d608a6d1fcd1d4b38e42bfd626fd23
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-01-05 23:10:21 +0000

    fix: dep script for FE

---
+++ frontend/dep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash tdesign.sh
+pnpm i
--- frontend/tdesign.sh
+++ frontend/tdesign.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd public
 rm tdesign.min.css
 rm tdesign.min.js
 wget https://unpkg.com/tdesign-react/dist/tdesign.min.css
 wget https://unpkg.com/tdesign-react/dist/tdesign.min.js
+cd ..
